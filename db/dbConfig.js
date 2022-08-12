be0f7fe4ae01507fd4f38f1695fda212b5a3f5d2
Alexander <alexanderl96@yahoo.com>
2022-08-12 09:02:08 -0400

    change server

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,26 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+// const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+const db_Url = process.env.DATABASE_URL;
 
-const cn = DATABASE_URL ? {
-  connectionString: DATABASE_URL,
-  max: 30,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-}:{
-  host: PG_HOST,
-  port: PG_PORT,
-  database: PG_DATABASE,
-  user: PG_USER,
-};
+const cn = {
+  connectionString: db_Url,
+  allowExitOnIdle: true,
+  max: 30
+}
+// const cn = DATABASE_URL ? {
+//   connectionString: DATABASE_URL,
+//   max: 30,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// }:{
+//   host: PG_HOST,
+//   port: PG_PORT,
+//   database: PG_DATABASE,
+//   user: PG_USER,
+// };
 
 const db = pgp(cn);
 
